8b959a47052a68c397c76ca63d51dacb1cdd5c7f
PJ <116917640+cypress3489@users.noreply.github.com>
2022-12-08 15:23:08 +0000

    commit

--- +Year.py
+++ +Year.py
@@ -1,11 +1,11 @@
 import datetime
 
-years = "16"
+years = 16
 time1 = datetime.date.today()
-time2 = time.day
-time3 = time.month
+time2 = time1.day
+time3 = time1.month
 
 
-# if add1 == 
-
-
+if time3 == 1:
+    if time2 == 1:
+        years += 1
